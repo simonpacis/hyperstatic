359ae005cef10635c7e142aaea21b929240bb1ab
Simon Pacis <simon@pacis.dk>
2022-06-25 08:11:32 -0700

    Documentation created

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,49 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\hs\u0028\u0029",
+            "name": "hs",
+            "summary": "If\u0020\u0024key\u0020is\u0020found\u0020in\u0020the\u0020associated\u0020.json\u002Dfile,\u0020it\u0020returns\u0020the\u0020value\u0020associated\u0020with\u0020it.",
+            "url": "namespaces/default.html#function_hs"
+        },                {
+            "fqsen": "\\hscontains\u0028\u0029",
+            "name": "hscontains",
+            "summary": "Returns\u0020keys\u0020\u0028and\u0020their\u0020values\u0029\u0020that\u0020contain\u0020\u0024substr\u0020in\u0020the\u0020associated\u0020.json\u002Dfile.",
+            "url": "namespaces/default.html#function_hscontains"
+        },                {
+            "fqsen": "\\hsexists\u0028\u0029",
+            "name": "hsexists",
+            "summary": "Returns\u0020true\u0020if\u0020a\u0020key\u0020in\u0020the\u0020associated\u0020.json\u002Dfile\u0020with\u0020\u0024key\u0020exists.",
+            "url": "namespaces/default.html#function_hsexists"
+        },                {
+            "fqsen": "\\hsc\u0028\u0029",
+            "name": "hsc",
+            "summary": "Returns\u0020the\u0020included\u0020hyperStatic\u0020Component\u0020found\u0020at\u0020\u0024component,\u0020and\u0020populates\u0020it\u0020with\u0020the\u0020data\u0020found\u0020at\u0020\u0024key\u0020in\u0020the\u0020assoicated\u0020.json\u002Dfile.",
+            "url": "namespaces/default.html#function_hsc"
+        },                {
+            "fqsen": "\\hscv\u0028\u0029",
+            "name": "hscv",
+            "summary": "Returns\u0020the\u0020component\u0020variable\u0020for\u0020the\u0020current\u0020component.\u0020Can\u0020only\u0020be\u0020called\u0020from\u0020within\u0020a\u0020component.",
+            "url": "namespaces/default.html#function_hscv"
+        },                {
+            "fqsen": "\\copyDirectory\u0028\u0029",
+            "name": "copyDirectory",
+            "summary": "Copies\u0020a\u0020directory\u0020and\u0020all\u0020of\u0020its\u0020contents\u0020from\u0020\u0024src\u0020to\u0020\u0024dest.",
+            "url": "namespaces/default.html#function_copyDirectory"
+        },                {
+            "fqsen": "\\isValidFile\u0028\u0029",
+            "name": "isValidFile",
+            "summary": "Checks\u0020whether\u0020a\u0020passed\u0020file\u0020is\u0020valid\u0020for\u0020our\u0020purposes.",
+            "url": "namespaces/default.html#function_isValidFile"
+        },                {
+            "fqsen": "\\parseFile\u0028\u0029",
+            "name": "parseFile",
+            "summary": "Compiles\u0020the\u0020passed\u0020.php\u002Dfile,\u0020so\u0020that\u0020the\u0020outputs\u0020of\u0020processing\u0020it\u0020can\u0020be\u0020stored\u0020as\u0020a\u0020string\u0020in\u0020a\u0020variable.",
+            "url": "namespaces/default.html#function_parseFile"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
--- hyperstatic.php
+++ hyperstatic.php
@@ -1,11 +1,7 @@
 <?php
 
-/*
- *
- *
- *	hyperStatic. A simple templating engine in PHP.
- *
- *
+/**
+ * hyperStatic. A simple templating engine in PHP.
  * */
 
 
@@ -17,24 +13,42 @@ $pure_dist_cwd = $raw_cwd . "/assets";
 $directory = new DirectoryIterator($src_cwd);
 $project_directory = new DirectoryIterator($raw_cwd);
 $data = null;
+$has_rsync = null;
 
 
-function hs($string)
+/**
+ * If $key is found in the associated .json-file, it returns the value associated with it.
+ *
+ * @param string $key The $key you are looking to get the value from in the associated .json-file.
+ *
+ * @return string|null The value associated with $key in the associated .json-file or null if nothing found.
+ */
+function hs($key)
 {
 	global $json;
-	if(array_key_exists($string, $json))
+	if(array_key_exists($key, $json))
 	{
-		return $json[$string];
+		return $json[$key];
 	}
 }
 
-function hscontains($string, $sort = false)
+/**
+ * Returns keys (and their values) that contain $substr in the associated .json-file.
+ *
+ * E.g. if you pass "navigation" as the $substr parameter, all entries in the associated .json-file that contain "navigation" will be returned.
+ *
+ * @param string $substr The substring to look for. 
+ * @param bool $sort Optional. Whether or not to sort the returned keys alphabetically. Defaults to false.
+ *
+ * @return array The entries in which the key contained $substr.
+ */
+function hscontains($substr, $sort = false)
 {
 	global $json;
 	$js_contains = [];
 	foreach($json as $key => $json_string)
 	{
-		if(strpos($key, $string) !== false)
+		if(strpos($key, $substr) !== false)
 		{
 			$js_contains[$key] = $json_string;
 		}
@@ -46,33 +60,63 @@ function hscontains($string, $sort = false)
 	}
 	return $js_contains;
 }
-
-function hsexists($string)
+/**
+ * Returns true if a key in the associated .json-file with $key exists.
+ *
+ * @param string $key The $key to look for.
+ *
+ * @return bool True if $key exists, false if not.
+ */
+function hsexists($key)
 {
 	global $json;
-	if(array_key_exists($string, $json))
+	if(array_key_exists($key, $json))
 	{
 		return true;
 	} else {
 		return false;
 	}
 }
-
-function hsc($string, $component)
+/**
+ * Returns the included hyperStatic Component found at $component, and populates it with the data found at $key in the assoicated .json-file.
+ *
+ * @param string $key The $key to populate the component with data from.
+ * @param string $component The path to the component that is to be included and pre-populated.
+ *
+ * @return string The included component pre-populated.
+ */
+function hsc($key, $component)
 {
 	global $json, $src_cwd, $data;
 
-	$GLOBALS['data'] = hs($string);
+	$GLOBALS['data'] = hs($key);
 	return include($src_cwd . "/" .  $component); 
 
 }
 
+/**
+ * Returns the component variable for the current component. Can only be called from within a component.
+ *
+ * @param string $variable Optional. The name of the variable stored in $GLOBALS. hsc() sets it to "data". Defaults to "data".
+ *
+ * @return mixed The component variable for the current component. 
+ */
 function hscv($variable = "data")
 {
 	return $GLOBALS[$variable];
 }
 
-$has_rsync = null;
+/**
+ * Copies a directory and all of its contents from $src to $dest.
+ *
+ * If your system has rsync installed it is possible to exclude a certain file or directory from being copied. If not, everything will be copied.
+ *
+ * @param string $src The path to copy files from.
+ * @param string $dest The path to copy files to.
+ * @param string $exclude Optional. Path to file or directory that is not to be copied. Defaults to "".
+ *
+ * @return void
+ */
 function copyDirectory($src, $dest, $exclude = "")
 {
 	global $has_rsync;
@@ -97,24 +141,36 @@ function copyDirectory($src, $dest, $exclude = "")
 	} else {
 		shell_exec("cp -r ". $src ." ". $dest);
 	}
-
-
-
-
 }
 
-function isValidFile(SplFileInfo $file_info)
+/**
+ * Checks whether a passed file is valid for our purposes.
+ *
+ * Being valid here means that it is a .php-file, not hyperstatic.php, and not a directory.
+ *
+ * @param SplFileInfo $file The file to evaluate.
+ *
+ * @return bool Whether the file is valid or not.
+ */
+function isValidFile(SplFileInfo $file)
 {
-	return $file_info->isFile()
-		&& 'php' === $file_info->getExtension()
-		&& basename(__FILE__) !== $file_info->getBasename();
+	return $file->isFile()
+		&& 'php' === $file->getExtension()
+		&& basename(__FILE__) !== $file->getBasename();
 }
 
-function parseFile(SplFileInfo $file_info)
+/**
+ * Compiles the passed .php-file, so that the outputs of processing it can be stored as a string in a variable.
+ *
+ * @param SplFileInfo $file The file to compile.
+ * 
+ * @return string The compiled .php-file.
+ */
+function parseFile(SplFileInfo $file)
 {
 	global $src_cwd;
 	ob_start();
-	include $src_cwd . "/" . $file_info->getBasename();
+	include $src_cwd . "/" . $file->getBasename();
 	$data = ob_get_contents();
 	ob_end_clean();
 
